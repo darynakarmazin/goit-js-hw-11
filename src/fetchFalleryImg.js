86a42f4bd47e976b7be7b74ba1d5b85ef934fe51
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-04-11 18:54:51 +0300

    Update fetch function

--- src/fetchFalleryImg.js
+++ src/fetchFalleryImg.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+const API_KEY = '35290900-56ffde2696ef97590bed2c34b';
+const URL = `https://pixabay.com/api/`;
+
 export class ApiFalleryImg {
   constructor() {
     this.searchQuery = '';
@@ -8,7 +11,7 @@ export class ApiFalleryImg {
   async fetchFalleryImg() {
     const param = {
       params: {
-        key: '35290900-56ffde2696ef97590bed2c34b',
+        key: API_KEY,
         q: this.searchQuery,
         image_type: 'photo',
         orientation: 'horizontal',
@@ -17,9 +20,8 @@ export class ApiFalleryImg {
         page: this.page,
       },
     };
-    const url = `https://pixabay.com/api/`;
     try {
-      const response = await axios.get(url, param);
+      const response = await axios.get(URL, param);
       this.incrementPage();
       return response.data.hits;
     } catch (error) {
@@ -27,11 +29,11 @@ export class ApiFalleryImg {
     }
   }
   incrementPage() {
-  this.page = this.page + 1;
-}
+    this.page = this.page + 1;
+  }
   resetPage() {
-  this.page = 1;
-}
+    this.page = 1;
+  }
 
   get query() {
     return this.searchQuery;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const apiService = new ApiFalleryImg();
 
 const searchForm = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
-const btnFindMore = document.querySelector('.find-more');
+const btnFindMore = document.querySelector('.load-more');
 
 searchForm.addEventListener('submit', onSearch);
 btnFindMore.addEventListener('click', onFindMore);
@@ -17,35 +17,85 @@ function onSearch(event) {
 
   apiService.query = event.target.elements.searchQuery.value;
   apiService.resetPage();
-  apiService.fetchFalleryImg().then(dataMarkup);
+  apiService
+    .fetchFalleryImg()
+    .then(hits => {
+      if (hits.length === 0) {
+        showError(error);
+      }
+      clearMarkup();
+      appendMarkup(hits);
+      showMessage(
+        'success',
+        'Hooray! We found ${response.data.totalHits} images.'
+      );
+    })
+    .catch(showError);
 }
 
 function onFindMore() {
-  apiService.fetchFalleryImg().then(dataMarkup);
+  apiService.fetchFalleryImg().then(hits => appendMarkup(hits));
 }
-function dataMarkup(hits) {
-  const dataMarkup = hits
-    .map(hits => {
-      return `
-  <div class="photo-card">
-  <img src="${hits.webformatURL}" alt="${hits.tags}" loading="lazy" />
+function appendMarkup(hits) {
+gallery.insertAdjacentHTML('beforeend', createDataMarkup(hits));
+};
+
+function createDataMarkup(hits) {
+  return (dataMarkup = hits
+    .map(
+      ({
+        largeImageURL,
+        webformatURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+  
+  <div class="gallery__item">
+  <a class="gallery__link" href="${largeImageURL}">
+  <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
-      <b>Likes: ${hits.likes}</b>
+      <b>Likes: ${likes}</b>
     </p>
     <p class="info-item">
-      <b>Views: ${hits.views}</b>
+      <b>Views: ${views}</b>
     </p>
     <p class="info-item">
-      <b>Comments: ${hits.comments}</b>
+      <b>Comments: ${comments}</b>
     </p>
     <p class="info-item">
-      <b>Downloads: ${hits.downloads}</b>
+      <b>Downloads: ${downloads}</b>
     </p>
-  </div>
 </div>
-  `;
-    })
-    .join(' ');
-  gallery.innerHTML = dataMarkup;
+</a>
+</div>
+`;
+      }
+    )
+    .join(' '));
 }
+
+function clearMarkup() {
+  gallery.innerHTML = '';
+}
+
+function showMessage(type, message) {
+  Notiflix.Notify[type](message);
+}
+
+function showError(error) {
+  showMessage(
+    'failure',
+    'Sorry, there are no images matching your search query. Please try again.'
+  );
+  clearMarkup();
+}
+
+var lightbox = new SimpleLightbox('.gallery__item a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
